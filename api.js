1550ad7f677c25e4c54cae4ba5d2244d5be779cb
Anton <Impl3x@yandex.ru>
2023-04-24 20:51:14 +0300

    first commit

---
+++ api.js
@@ -0,0 +1,80 @@
+// "функция fetch вызывается только в этом модуле;" по ДЗ
+// import renderComments from "./renderComments.js";
+// const buttonElement = document.getElementById("add-button");
+// const nameInputElement = document.getElementById("name-input");
+// const textInputElement = document.getElementById("text-input");
+
+const host = "https://webdev-hw-api.vercel.app/api/v2/anton-sobachkin/comments";
+export function getComms({
+  token
+}) {
+  return fetch(host, {
+    method: "GET",
+    headers: {
+      Authorization: token,
+    },
+  }).then((res) => {
+    if (res === 401) {
+      throw new Error("Нет авторизации");
+    }
+    return res.json();
+  });
+}
+
+export function sendComm({
+  text,
+  token
+}) {
+  return fetch(host, {
+    method: "POST",
+    body: JSON.stringify({
+      text,
+      // "text": textInputElement.value,
+      // "name": nameInputElement.value,
+      //   forceError: true,
+    }),
+    headers: {
+      Authorization: token,
+    },
+  });
+}
+
+
+//https://github.com/GlebkaF/webdev-hw-api/blob/main/pages/api/user/README.md
+export function loginUser({
+  login,
+  password
+}) {
+  return fetch("https://webdev-hw-api.vercel.app/api/user/login", {
+    method: "POST",
+    body: JSON.stringify({
+      login,
+      password,
+    }),
+  }).then((response) => {
+    if (response.status === 400){
+      throw new Error('неверный логин или пароль');
+    }
+    return response.json();
+  });
+}
+
+export function regUser({
+  login,
+  password,
+  name,
+}) {
+  return fetch("https://webdev-hw-api.vercel.app/api/user", {
+    method: "POST",
+    body: JSON.stringify({
+      login,
+      password,
+      name,
+    }),
+  }).then((response) => {
+    if (response.status === 400){
+      throw new Error('пользователь с таким логином уже сущетсвует');
+    }
+    return response.json();
+  });
+}
---
+++ script.js
@@ -0,0 +1,144 @@
+// fetchTodosAndRender = codeAdjust
+// renderApp = renderComments
+// getTodos = getComms
+// addTodo = sendComm`
+// renderLoginComponent = renderLogin
+import {
+  getComms
+} from "./api.js";
+import {
+  sendComm
+} from "./api.js";
+import {
+  renderLogin
+} from "./components/login-component.js";
+let comments = [];
+let user = null;
+let token = null;
+const codeAdjust = (token) => {
+  return getComms({
+    token
+  }).then((responseData) => {
+    const appComments = responseData.comments.map((comment) => {
+      return {
+        name: comment.author.name,
+        date: new Date(comment.date).toLocaleString(),
+        text: comment.text,
+        likes: comment.likes,
+        isLiked: comment.isLiked,
+        id: comment.id,
+      };
+    });
+    comments = appComments;
+    renderComments();
+  });
+};
+codeAdjust();
+const renderComments = () => {
+  const appEl = document.getElementById('app');
+  //  if (!token) { 
+  //   renderLogin({
+  //     appEl,
+  //     setToken: (newToken) => {
+  //       token = newToken;
+  //     },
+  //     setUser: (newUser) => {
+  //       user = newUser;
+  //     },
+  //     codeAdjust,
+  //   });
+  //   return;
+  // }
+  const commentsHtml = comments.map((comment, index) => {
+    return `<li class="comment">
+    <div class="comment-header">
+      <div class="comment-name">${comment.name} </div>
+      <div>${comment.date}</div>
+    </div>
+    <div class="comment-body">
+      <div class="comment-text" >
+        ${comment.text}
+      </div>
+    </div>
+    <div class="comment-footer">
+      <div class="likes">
+        <span class="likes-counter">${comment.likes}</span>      
+        <button class="like-button ${comment.isLiked}" data-like="${index}"></button>
+      </div>
+    </div>
+  </li>`;
+  }).join('');
+
+
+  const appHtml = `  
+  <div class="container">
+      <ul class="comments" id="list">
+      ${commentsHtml}
+    </ul>
+    ${token ? `<div class="add-form">
+      <input type="text" class="add-form-name" placeholder="Введите имя" id="name-input" disabled value="${user.name}"/>
+      <textarea type="textarea" class="add-form-text" placeholder="Введите ваш коментарий" rows="4"
+        id="text-input" ></textarea>
+      <div class="add-form-row">
+        <button class="add-form-button turn-off" id="add-button">Написать</button>
+      </div>
+
+    </div>
+  </div>`: `<p>Чтобы добавить комментарий, <a class="check" id="auth">авторизуйтесь</a></p>`}`;
+
+  appEl.innerHTML = appHtml;
+
+  if (!token) {
+
+    document.getElementById("auth").addEventListener('click', () => {
+      renderLogin({
+        appEl,
+        setToken: (newToken) => {
+          token = newToken;
+        },
+        setUser: (newUser) => {
+                user = newUser;
+              },
+        codeAdjust
+      });
+    });
+    return;
+  }
+
+  const buttonElement = document.getElementById("add-button");
+  const textInputElement = document.getElementById("text-input");
+  const nameInputElement = document.getElementById('name-input');
+  nameInputElement.value = user.name;
+  // const listElement = document.getElementById("list");
+  // const deleteButton = document.getElementById("del-button");
+
+  //Добавление комментария
+
+  buttonElement.addEventListener("click", () => {
+    if (textInputElement.value === "") {
+      return;
+    }
+    buttonElement.disabled = true;
+    buttonElement.textContent = "Элемент добавлятся...";
+    sendComm({
+      "text": textInputElement.value,
+      // text,
+      token
+    })
+    .then(() => {
+      return codeAdjust();
+    });
+  });
+
+  // Обновление по кнопке
+  function addNewElement() {
+    buttonElement.addEventListener('click', () => {
+      codeAdjust();
+    });
+  }
+  addNewElement();
+
+};
+// renderComments();
+// getComms();
+codeAdjust();
